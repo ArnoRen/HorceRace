d46a1d64d9f5b1779ca75d214439f8026f1a1d7d
ArnoRen <161221311+ArnoRen@users.noreply.github.com>
2024-10-13 18:53:31 +0000

    0.0.2

---
+++ Horse.py
@@ -0,0 +1,16 @@
+def test():
+    print("Horse Working.")
+
+class Horse:
+    def __init__(self, name, star, rider, type):
+        self.name = name
+        self.star = star
+        self.rider = rider
+        self.type = type
+
+class Math:
+    @staticmethod
+    def add5 (x):
+        return x+5
+
+print(Math.add5(5))
